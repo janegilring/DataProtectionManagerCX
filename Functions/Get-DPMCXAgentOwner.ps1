3adc1806cdb036c400a56d1e8084bdea6d09a762
Jan Egil Ring <jan.egil.ring@outlook.com>
2016-10-02 12:13:12 +0200

    Add pipeline support for -Computername Add alias for -ComputerName Add more verbose messages Bugfix (remove $session properly when connection fails)

--- Functions/Get-DPMCXAgentOwner.ps1
+++ Functions/Get-DPMCXAgentOwner.ps1
@@ -4,36 +4,56 @@ function Get-DPMCXAgentOwner
   [CmdletBinding()]
   param (
     [Parameter(Position = 0, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+    [Alias('__Server','CN')]
     [ValidateNotNullOrEmpty()]
     [string[]] $ComputerName = 'localhost'
   )
 
+Process {
+
+foreach ($Computer in $ComputerName) {
+
   try 
   {
-    $session = New-PSSession -ComputerName $ComputerName -ErrorAction Stop
+    $session = New-PSSession -ComputerName $Computer -ErrorAction Stop
 
+    Write-Verbose -Message "Connected to $Computer via PowerShell remoting, gathering DPM information..."
 
   }
 
   catch 
   {
 
-    Write-Warning $_.Exception
+    Write-Verbose -Message "Failed to connect to $Computer via PowerShell remoting..."
+
+    Write-Verbose $_.Exception
+
+    # Todo: Output object here?
+
+      if ($session) 
+          {
+            Remove-Variable -Name session
+          }
 
   }
 
   if ($session) 
   {
     $DPMAgentIsInstalled = Invoke-Command -Session $session -ScriptBlock {
+
+      $VerbosePreference = $using:VerbosePreference
+
       Test-Path -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft Data Protection Manager'
-    }
+
+          }
 
     if ($DPMAgentIsInstalled) 
     {
       
-      Write-Verbose -Message 'DPM is installed, finding Active Owner files...'
+      Write-Verbose -Message "DPM is installed on $Computer, finding Active Owner files..."
 
       $ActiveOwnerInfo    = Invoke-Command -Session $session -ScriptBlock {
+
         try 
         {
           $Path = (Get-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Microsoft Data Protection Manager\Setup' -ErrorAction Stop).InstallPath
@@ -53,6 +73,8 @@ function Get-DPMCXAgentOwner
         foreach ($thisPath in $ActiveOwnerFilePaths) 
         {
 
+           Write-Verbose -Message "Processing ActiveOwner file $thisPath"
+
           # Todo: Read binary file contents
 
         }
@@ -67,6 +89,11 @@ function Get-DPMCXAgentOwner
     }
 
     Remove-PSSession -Session $session
+
+    }
+ 
   }
+
+ }
 
 }
